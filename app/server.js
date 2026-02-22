2d1439fc69d8a0360ba802c5ca8d736a69bc89d2
kashyapvaibhav123 <kashyapvaibhav1244@gmail.com>
2026-02-22 16:53:10 +0530

    Added logging functionality in the server.js

--- app/server.js
+++ app/server.js
@@ -1,13 +1,29 @@
+require('dotenv').config();
+
 const express=require('express');
 const app =express();
+const PORT=process.env.PORT ||80;
+const fs=require('fs');
+const MESSAGE=process.env.MESSAGE || "CLOUD IS RUNNING"
+const LOG_FILE="/var/log/myapp.log";
+function logMessage(message) {
+    const log = `${new Date().toISOString()} - ${message}\n`;
+    fs.appendFileSync(LOG_FILE,log);
+}
 
 app.get('/',(req,res)=>{
-    res.send("Updated via CI/CD");
+    logMessage("Root endpoint accessed");
+    res.send(MESSAGE);
 });
 
 app.get('/health',(req,res)=>{
-    res.json({status:"Ok Health"});
+    logMessage("Health endpoint accessed");
+    res.json({status:"OK"});
 });
 app.listen(80,()=>{
     console.log("Server Start on port 80")
+});
+app.listen(prompt,()=>{
+    logMessage(`Server started on port ${PORT}`);
+    console.log(`Server started on port ${PORT}`);
 });
