cf334bafe7209891d19364cf23e27ac8287c2fe4
kashyapvaibhav123 <kashyapvaibhav1244@gmail.com>
2026-02-22 16:02:57 +0530

    Test CI/CD Automation

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ const express=require('express');
 const app =express();
 
 app.get('/',(req,res)=>{
-    res.send("Cloud App Running");
+    res.send("Updated via CI/CD");
 });
 
 app.get('/health',(req,res)=>{
